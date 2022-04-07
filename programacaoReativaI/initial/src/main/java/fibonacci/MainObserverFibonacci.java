3a57810d369b37467654f227dc0e20a208fc818e
carvalheirafc <carvalheirafc@gmail.com>
2022-04-07 14:40:43 -0300

    removed unused target files, need to fix gitignore to it, and added pom.xml file

--- programacaoReativaI/initial/src/main/java/fibonacci/MainObserverFibonacci.java
+++ programacaoReativaI/initial/src/main/java/fibonacci/MainObserverFibonacci.java
@@ -1,1 +1,21 @@
-package fibonacci
+package imd.ufrn.br;
+
+import java.util.Observer;
+import java.util.Subject;
+
+public class MainObserverFibonacci {
+	public static void main(String[] args) {
+		Subject<String> subject = new ConcreteSubject();
+		Observer<String> observerAlpha = new ConcreteOserverA();
+		Observer<String> observerBeta = new ConcreteOserverB();
+		Fibonacci fibo = new Fibonacci();
+
+		subject.registerObserver(observerAlpha);
+		subject.registerObserver(observerBeta);
+
+		for(int ii=0; ii<50; ii++){
+			subject.notifyObservers("Fibonacci("+i+")"+fibo.fibo(i));
+		}
+
+	}
+}
